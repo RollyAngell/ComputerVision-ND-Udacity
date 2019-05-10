d5f7d445ab5ba9c326fccafefd81fcddeae4488a
RollyAngell <angel.inga.v@uni.pe>
2019-05-10 11:47:45 -0500

    update model.py

--- Projects/P1_Facial_Keypoints/models.py
+++ Projects/P1_Facial_Keypoints/models.py
@@ -29,12 +29,60 @@ class Net(nn.Module):
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         self.pool = nn.MaxPool2d(kernel = 2, stride = 2)
         
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(in_features = 36864, out_features = 1000) # The number of input gained by "print("Flatten size: ", x.shape)" in below
+        self.fc2 = nn.Linear(in_features = 1000,    out_features = 1000)
+        self.fc3 = nn.Linear(in_features = 1000,    out_features = 136) # the output 136 in order to having 2 for each of the 68 keypoint (x, y) pairs
+
+        # Dropouts
+        self.drop1 = nn.Dropout(p = 0.1)
+        self.drop2 = nn.Dropout(p = 0.2)
+        self.drop3 = nn.Dropout(p = 0.3)
+        self.drop4 = nn.Dropout(p = 0.4)
+        self.drop5 = nn.Dropout(p = 0.5)
+        self.drop6 = nn.Dropout(p = 0.6)
+        
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        
+                # First - Convolution + Activation + Pooling + Dropout
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x = self.drop1(x)
+        #print("First size: ", x.shape)
+
+        # Second - Convolution + Activation + Pooling + Dropout
+        x = self.drop2(self.pool(F.relu(self.conv2(x))))
+        #print("Second size: ", x.shape)
+
+        # Third - Convolution + Activation + Pooling + Dropout
+        x = self.drop3(self.pool(F.relu(self.conv3(x))))
+        #print("Third size: ", x.shape)
+
+        # Forth - Convolution + Activation + Pooling + Dropout
+        x = self.drop4(self.pool(F.relu(self.conv4(x))))
+        #print("Forth size: ", x.shape)
+
+        # Flattening the layer
+        x = x.view(x.size(0), -1)
+        #print("Flatten size: ", x.shape)
+
+        # First - Dense + Activation + Dropout
+        x = self.drop5(F.relu(self.fc1(x)))
+        #print("First dense size: ", x.shape)
+
+        # Second - Dense + Activation + Dropout
+        x = self.drop6(F.relu(self.fc2(x)))
+        #print("Second dense size: ", x.shape)
+
+        # Final Dense Layer
+        x = self.fc3(x)
+        #print("Final dense size: ", x.shape)
         
         
         # a modified x, having gone through all the layers of your model, should be returned
